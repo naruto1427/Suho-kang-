c710bf8dfd5d18f2ac1d8cd6cf8f800d1492f558
naruto1427 <pritnaruto@gmail.com>
2025-05-20 00:02:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,10 @@ from plugins.cb_data import app as Client2
 from config import *
 import pyromod
 import pyrogram.utils
+from threading import Thread
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
+Thread(target=run_dummy_server).start()
 pyrogram.utils.MIN_CHAT_ID = -999999999999
 pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
@@ -11,7 +14,15 @@ pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
 bot = Client("Renamer", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
 
+class DummyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running!")
 
+def run_dummy_server():
+    server = HTTPServer(("0.0.0.0", 8080), DummyHandler)
+    server.serve_forever()
 
 
 if STRING_SESSION:
